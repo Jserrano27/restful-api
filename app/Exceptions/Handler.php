5789bcb36489c03045c8248da5d197bd124a89cd
Joaquin <jserrano.vd@gmail.com>
2019-09-15 19:03:03 +0000

    Defining NotFound HTTP Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Auth\AuthenticationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -67,6 +68,10 @@ class Handler extends ExceptionHandler
 
         if($exception instanceof AuthorizationException){
             return $this->errorResponse($exception->getMessage(), 403);
+        }
+
+        if($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse("The specified URL cannot be found", 404);
         }
 
         return parent::render($request, $exception);
