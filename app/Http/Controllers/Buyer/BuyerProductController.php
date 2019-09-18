4d3b166166a6dc3d52313065a07f7c69e67143a2
Joaquin <jserrano.vd@gmail.com>
2019-09-18 11:25:41 +0000

    Implementing Complex Controllers for Buyer

---
+++ app/Http/Controllers/Buyer/BuyerProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        /* This wont work: A buyer has many transactions, so the collection "transactions"
+        ** can't find one attribute for a collection. The solution: Eager Loading
+        ** $products = $buyer->transactions->product;
+        */
+
+        $products = $buyer->transactions()
+        ->with('product')
+        ->get()
+        ->pluck('product'); // Retrieves only the values for the products (exclude values of the transactions)
+
+        return $this->showAll($products);
+    }
+}
---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()->with('product.seller')
+        ->get()                     
+        ->pluck('product.seller')   //show only sellers inside of products, inside of transaction collection
+        ->unique('id') //remove sellers with repeated id 
+        ->values(); // recreate de list to remove empty values of repeated sellers
+
+        return $this->showAll($sellers);
+    }
+
+}
