e5e9240003e97dba539eb4f41454be8d2acac9cf
Joaquin <jserrano.vd@gmail.com>
2019-10-03 11:59:34 +0000

    Implementing HATEOAS for Buyer Instances

--- app/Transformers/BuyerTransformer.php
+++ app/Transformers/BuyerTransformer.php
@@ -21,7 +21,40 @@ class BuyerTransformer extends TransformerAbstract
             'is_Verified' => (int)$buyer->verified,
             'creation_Date' => (string)$buyer->created_at,
             'last_Change' => (string)$buyer->updated_at,
-            'deleted_Date' => (isset($buyer->deleted_at) ? (string)$buyer->deleted_at : null)
+            'deleted_Date' => (isset($buyer->deleted_at) ? (string)$buyer->deleted_at : null),
+
+            'links' =>[
+                [
+                    'href' => route('buyers.show', $buyer->id),
+                    'rel' => 'self',
+                    'type' => 'GET'
+                ],
+                [
+                    'href' => route('buyers.categories.index', $buyer->id),
+                    'rel' => 'buyers.categories',
+                    'type' => 'GET'
+                ],
+                [
+                    'href' => route('buyers.products.index', $buyer->id),
+                    'rel' => 'buyers.products',
+                    'type' => 'GET'
+                ],
+                [
+                    'href' => route('buyers.sellers.index', $buyer->id),
+                    'rel' => 'buyers.sellers',
+                    'type' => 'GET'
+                ],
+                [
+                    'href' => route('buyers.transactions.index', $buyer->id),
+                    'rel' => 'buyers.transactions',
+                    'type' => 'GET'
+                ],
+                [
+                    'href' => route('users.show', $buyer->id),
+                    'rel' => 'user.profile',
+                    'type' => 'GET'
+                ],
+            ],
         ];
     }
 
