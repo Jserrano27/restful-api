f9798775c090afda73e26653f95df6de465333f0
Joaquin Serrano <jserrano.vd@gmail.com>
2019-10-24 11:13:33 +0000

    Adding CORS to error responses too

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Asm89\Stack\CorsService;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
@@ -59,6 +60,16 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        $response = $this->handleException($request, $exception);
+
+        app(CorsService::class)->addActualRequestHeaders($response, $request);
+
+        return $response;
+        
+    }
+
+    public function handleException($request, Exception $exception) {
+
         if($exception instanceof ValidationException){
             return $this->convertValidationExceptionToResponse($exception, $request);
         } 
@@ -107,6 +118,7 @@ class Handler extends ExceptionHandler
         }
 
         return $this->errorResponse("Unexpected error. Please, try again later", 500);
+
     }
 
     /**
